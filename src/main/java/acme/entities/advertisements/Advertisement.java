6b31d50dface9f1aefc394bd0416d16147de3690
sardongui <saradg1995@gmail.com>
2020-10-25 19:06:56 +0100

    arreglos

--- src/main/java/acme/entities/advertisements/Advertisement.java
+++ src/main/java/acme/entities/advertisements/Advertisement.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
